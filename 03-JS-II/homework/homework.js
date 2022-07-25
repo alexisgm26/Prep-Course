01f0b7c4f7ba50b3138e8a42cea6f7481ea33c3f
alexisgm26 <alexis.g.menes@gmail.com>
2022-07-25 10:21:27 -0300

    first commit

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -1,6 +1,10 @@
 // No cambies los nombres de las funciones.
 
 function obtenerMayor(x, y) {
+  if (x > y) { return x ;
+} else if (x < y) { return y;
+} else if (x === y) { return y;
+}
   // "x" e "y" son números enteros (int).
   // Devuelve el número más grande
   // Si son iguales, devuelve cualquiera de los dos
@@ -8,12 +12,22 @@ function obtenerMayor(x, y) {
 }
 
 function mayoriaDeEdad(edad) {
+  if (edad < 18){ return "Not allowed";
+} else if (edad >=18){ return "Allowed";
+}
   //Determinar si la persona según su edad puede ingresar a un evento.
   //Si tiene 18 años ó más, devolver --> "Allowed"
   //Si es menor, devolver --> "Not allowed"
 }
   
 function conection(status) {
+  if (status === 1) {
+    return "Online" ; 
+  } else if (status === 2){
+    return "Away" ;
+  } else 
+  { return "Offline";
+  }
   //Recibimos un estado de conexión de un usuario representado por un valor numérico. 
   //Cuando el estado es igual a 1, el usuario está "Online"
   //Cuando el estado es igual a 2, el usuario está "Away"
@@ -22,6 +36,15 @@ function conection(status) {
 }
 
 function saludo(idioma) {
+  if (idioma === "aleman" ) {
+    return "Guten Tag!";
+  } else if (idioma === "mandarin") {
+    return "Ni Hao!";
+  } else if (idioma === "ingles"){
+    return "Hello!" ;
+  } else {
+    return "Hola!";
+  }
   // Devuelve un saludo en tres diferentes lenguajes:
   // Si "idioma" es "aleman", devuelve "Guten Tag!"
   // Si "idioma" es "mandarin", devuelve "Ni Hao!"
@@ -31,6 +54,22 @@ function saludo(idioma) {
 }
 
 function colors(color) {
+  switch (color) {
+    case "blue": return "This is blue";
+      
+      break;
+    case "red": return "This is red";
+      break;
+    case "green":return "This is green";
+      break;
+    case "orange":return "This is orange"  
+      break;
+    
+  
+    default: return "Color not found";
+      break;
+  }
+
   //La función recibe un color. Devolver el string correspondiente:
   //En caso que el color recibido sea "blue", devuleve --> "This is blue"
   //En caso que el color recibido sea "red", devuleve --> "This is red"
@@ -41,18 +80,21 @@ function colors(color) {
 }
 
 function esDiezOCinco(numero) {
+   return numero === 10||numero === 5 ;
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
 }
 
 function estaEnRango(numero) {
+    return numero < 50 && numero > 20 ;
   // Devuelve "true" si "numero" es menor que 50 y mayor que 20
   // De lo contrario, devuelve "false"
   // Tu código:
 }
 
 function esEntero(numero) {
+  
   // Devuelve "true" si "numero" es un entero (int/integer)
   // Ejemplo: 0.8 -> false
   // Ejemplo: 1 -> true
@@ -87,6 +129,7 @@ function esPrimo(numero) {
 }
 
 function esVerdadero(valor){
+
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
@@ -107,6 +150,7 @@ function tieneTresDigitos(numero){
 }
 
 function doWhile(numero) {
+  
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
